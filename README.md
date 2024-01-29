f5cb365de6853f48efe37e5aae9a804de52c7656
Abhishek Gupta <abhishek.gupta1115@gmail.com>
2024-01-29 13:03:06 -0500

    Create tasks using worker code

--- README.md
+++ README.md
@@ -14,20 +14,60 @@ https://www.python.org/downloads/
 pip3 install -r requirements.txt
 ```
 
-## Create Workflow
+## Setup
+Setup authentication using the below link and store environment variables in a .env file.
 
+https://github.com/conductor-sdk/conductor-python?tab=readme-ov-file#setup-sdk
+
+## App Structure
+
+This app has 6 components:
+1. Flask server that servers the booking endpoint to trigger booking workflow
+2. Booking Service
+3. Assignment Service
+4. Payment Service
+5. Notification Service
+6. Workflow definition creation script
+
+## Create Workflow
 ```shell
 python3 workflows.py
 ```
 
 ## Run Cab Booking Server
-
 ```shell
 python3 main.py
 ```
 
 # Run microservice based workers
-
 ```shell
 docker compose up
+```
+
+## Trigger Booking Creation
+
+### Booking Creation Success: Happy Path
+
+```curl
+curl --location 'http://127.0.0.1:5000/booking' \
+--header 'Content-Type: application/json' \
+--data '{
+    "pickUpLocation": "15 Mark Chester Pl, Chicago, IL 12045",
+    "dropOffLocation": "64 West 21st Street, Chicago, IL 13012",
+    "riderId": 1
+}'
+```
+
+### Booking Creation Failure: Distributed Rollback
+
+To simulate a distributed rollback in a Saga based microservice architecture, we make the payment fail for all riders with id > 2 which leads to terminate the main workflow and trigger a failure workflow.
+
+```curl
+curl --location 'http://127.0.0.1:5000/booking' \
+--header 'Content-Type: application/json' \
+--data '{
+    "pickUpLocation": "15 Mark Chester Pl, Chicago, IL 12045",
+    "dropOffLocation": "64 West 21st Street, Chicago, IL 13012",
+    "riderId": 3
+}'
 ```
--- workflow.py
+++ workflow.py
@@ -4,64 +4,62 @@ from conductor.client.workflow.conductor_workflow import ConductorWorkflow
 from conductor.client.workflow.executor.workflow_executor import WorkflowExecutor
 from conductor.client.workflow.task.simple_task import SimpleTask
 from conductor.client.workflow.task.fork_task import ForkTask
-from conductor.client.workflow.task.fork_task import JoinTask
+from services.booking_service.booking import book_ride, confirm_booking
+from services.assignment_service.assignment import assign_driver
+from services.payment_service.payments import make_payment
+from services.notification_service.notification import notify_customer, notify_driver
 
 def register_cab_booking_workflow() -> ConductorWorkflow:
     configuration = Configuration()
         
     workflow_executor = WorkflowExecutor(configuration=configuration)
 
-    book_ride = SimpleTask(task_def_name='book_ride', task_reference_name='book_ride_ref')
-    book_ride.input_parameters = {
-        'rider_id' : '${workflow.input.rider_id}',
-        'pick_up_location' : '${workflow.input.pick_up_location}',
-        'drop_off_location' : '${workflow.input.drop_off_location}'
-    }
+    workflow = ConductorWorkflow(
+        name='cab_service_saga_demo_3', description="Cab Service Saga Demo", executor=workflow_executor
+    )
+
+    book = book_ride(
+        task_ref_name='book_ride_ref',
+        pick_up_location=workflow.input('pick_up_location'),
+        drop_off_location=workflow.input('drop_off_location'),
+        rider_id=workflow.input('rider_id'),
+    )
     
-    assign_driver = SimpleTask(task_def_name='assign_driver', task_reference_name='assign_driver_ref')
-    assign_driver.input_parameters = {
-        'booking_id': '${book_ride_ref.output.booking_id}'
-    }
+    assign = assign_driver(task_ref_name='assign_driver_ref', booking_id=book.output('booking_id'))
     
-    make_payment = SimpleTask(task_def_name='make_payment', task_reference_name='make_payment_ref')
-    make_payment.input_parameters = {
-        'rider_id' : '${workflow.input.rider_id}',
-        'booking_id': '${book_ride_ref.output.booking_id}'
-    }
+    pay = make_payment(
+        task_ref_name='make_payment_ref',
+        rider_id=workflow.input('rider_id'),
+        booking_id=book.output('booking_id')
+    )
     
-    confirm_booking = SimpleTask(task_def_name='confirm_booking', task_reference_name='confirm_booking_ref')
-    confirm_booking.input_parameters = {
-        'booking_id': '${book_ride_ref.output.booking_id}',
-        'driver_id': '${assign_driver_ref.output.driver_id}'
+    confirm = confirm_booking(
+        task_ref_name='confirm_booking_ref',
+        booking_id=book.output('booking_id'),
+        driver_id=assign.output('driver_id')
+    )
+
+    notification_input_params = {
+        'booking_id': book.output('booking_id'),
+        'driver_id': assign.output('driver_id'),
+        'rider_id' : workflow.input('rider_id'),
+        'from' : workflow.input('pick_up_location'),
+        'to' : workflow.input('drop_off_location')
     }
 
-    notification_input_parameters = {
-        'booking_id': '${book_ride_ref.output.booking_id}',
-        'driver_id': '${assign_driver_ref.output.driver_id}',
-        'rider_id' : '${workflow.input.rider_id}',
-        'from' : '${workflow.input.pick_up_location}',
-        'to' : '${workflow.input.drop_off_location}'
-    }
-
-    notify_driver = SimpleTask(task_def_name='notify_driver', task_reference_name='notify_driver')
-    notify_driver.input_parameters = notification_input_parameters
-    notify_driver.optional = True
+    notify_d = notify_driver(task_ref_name='notify_driver_ref', **notification_input_params)
+    notify_d.optional = True
     
-    notify_customer = SimpleTask(task_def_name='notify_customer', task_reference_name='notify_customer')
-    notify_customer.input_parameters = notification_input_parameters
-    notify_customer.optional = True
+    notify_c = notify_customer(task_ref_name='notify_customer_ref', **notification_input_params)
+    notify_c.optional = True
     
-    send_notifications = ForkTask(
+    notify = ForkTask(
         task_ref_name='send_notifications',
-        forked_tasks=[[notify_driver], [notify_customer]],
+        forked_tasks=[[notify_d], [notify_c]],
         join_on=[]
     )
 
-    workflow = ConductorWorkflow(
-        name='cab_service_saga_demo', description="Cab Service Saga Demo", executor=workflow_executor
-    )
-        
-    workflow >> book_ride >> assign_driver  >> make_payment  >> confirm_booking >> send_notifications
+    workflow >> book >> assign  >> pay  >> confirm >> notify
     
     workflow.input_parameters(['pick_up_location', 'drop_off_location', 'rider_id'])
     workflow.output_parameters({'booking_id': '${book_ride_ref.output.booking_id}'})
